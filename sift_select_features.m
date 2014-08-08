8d233f0136502e0916e68b427411eb3a795f6435
Sang Phan <plsang@nii.ac.jp>
2014-08-08 21:00:02 +0900

    load feature in block of 1M

--- sift_select_features.m
+++ sift_select_features.m
@@ -13,9 +13,9 @@ function sift_select_features( sift_algo, param, version )
 	
 	proj_dir = '/net/per610a/export/das11f/plsang/trecvidmed14';
     max_features = 100000000; % 100M
-	video_sampling_rate = 0.5;
-    sample_length = 25; % frames
-    ensure_coef = 1.1;
+	video_sampling_rate = 1;
+    sample_length = 32; % frames
+    ensure_coef = 1.01;
 	
 	configs = set_global_config();
 	logfile = sprintf('%s/%s.log', configs.logdir, mfilename);
@@ -40,8 +40,7 @@ function sift_select_features( sift_algo, param, version )
 	
 	max_features_per_video = ceil(ensure_coef * max_features/length(selected_videos));
     
-    data = cell(length(selected_videos), 1);
-	location = cell(length(selected_videos), 1);
+	fprintf('Initialize data...\n');
 
 	output_file = sprintf('%s/feature/bow.codebook.devel/%s.%s.%s.sift/data/selected_feats_%d.hdf5', proj_dir, sift_algo, num2str(param), version, max_features);
 	output_location_file = sprintf('%s/feature/bow.codebook.devel/%s.%s.%s.sift/data/selected_location_%d.mat', proj_dir, sift_algo, num2str(param), version, max_features);
@@ -50,7 +49,20 @@ function sift_select_features( sift_algo, param, version )
 		return;
 	end
 	
-	%matlabpool open;
+	BLOCK_SIZE = 1000000; 
+	
+	% make sure data have enough capacity, each time added an amount of tmp_data (below).  
+	% will be stopped when size larger than max_features
+	data = zeros(128, max_features + BLOCK_SIZE + max_features_per_video, 'single');
+	location = zeros(6, max_features + BLOCK_SIZE + max_features_per_video, 'single');
+	dataPtr = 1;
+	
+	% make sure tmp_data have enough capacity, each time added an amount of max_features_per_video.  
+	% will be stopped when size larger than BLOCK_SIZE
+	tmp_data = zeros(128, BLOCK_SIZE + max_features_per_video, 'single');	
+	tmp_location = zeros(6, BLOCK_SIZE + max_features_per_video, 'single');
+	listPtr = 1;
+	
     for ii = 1:length(selected_videos),
         video_name = selected_videos{ii};
         
@@ -70,11 +82,13 @@ function sift_select_features( sift_algo, param, version )
 			selected_idx = selected_idx(rand_idx(1:sample_length));
 		end
 		
-		fprintf('Computing features for: %d - %s %f %% complete\n', ii, video_name, ii/length(selected_videos)*100.00);
-		feat = [];
-		frame = [];
-		for jj = selected_idx,
-			img_name = kfs(jj).name;
+		fprintf('[%d/%d] Computing features for: %s (%f %% complete)\n', ii, length(selected_videos), video_name, ii/length(selected_videos)*100.00);
+		feat = cell(length(selected_idx), 1);
+		frame = cell(length(selected_idx), 1);
+		
+		parfor jj = 1:length(selected_idx),
+			
+			img_name = kfs(selected_idx(jj)).name;
 			img_path = fullfile(video_kf_dir, img_name);
 			
 			[frames, descrs] = sift_extract_features( img_path, sift_algo, param );
@@ -84,37 +98,61 @@ function sift_select_features( sift_algo, param, version )
                 %warning('Maybe blank image...[%s]. Skipped!\n', img_name);
                 continue;
             end
-			feat = [feat descrs];
-			frame = [frame frames];
+			
+			feat{jj} = descrs;
+			frame{jj} = frames;
 		end
-        
-		%size(feat, 2)
+		
+		feat = cat(2, feat{:});
+		frame = cat(2, frame{:});
 		
         if size(feat, 2) > max_features_per_video,
-            [data{ii}, sel] = vl_colsubset(feat, max_features_per_video);
-			location{ii} = frame(:, sel);
+            
+			[tmp_data(:, listPtr:listPtr+max_features_per_video-1), sel] = vl_colsubset(feat, max_features_per_video);
+			listPtr=listPtr+max_features_per_video;
+			tmp_location(:, listPtr:listPtr+max_features_per_video-1) = frame(:, sel);
         else
-            data{ii} = feat;
-			location{ii} = frame;
+			tmp_length = size(feat, 2);
+			tmp_data(:, listPtr:listPtr+tmp_length-1) = feat;
+			tmp_location(:, listPtr:listPtr+tmp_length-1) = frame;
+			
+			listPtr=listPtr+tmp_length;
         end
         
+		if listPtr > BLOCK_SIZE,
+			
+			data(:, dataPtr:dataPtr+listPtr-2) = tmp_data(:, 1:listPtr-1);
+			location(:, dataPtr:dataPtr+listPtr-2) = tmp_location(:, 1:listPtr-1);
+			dataPtr = dataPtr + listPtr - 1;
+			
+			tmp_data(:,:) = 0;
+			tmp_location(:,:) = 0;
+			listPtr = 1;
+			
+			if dataPtr > max_features,
+				data(:, dataPtr:end) = [];
+				location(:, dataPtr:end) = [];
+				dataPtr = max_features + 1;
+				break;
+			end
+			
+			fprintf('... [%d/%d] features loaded \n', dataPtr - 1, max_features);
+		end
+		
     end
     
-	%matlabpool close;
-	
-    % concatenate features into a single matrix
-    data = cat(2, data{:});
-	location = cat(2, location{:});
-    
-    if size(data, 2) > max_features,
-         data = vl_colsubset(data, max_features);
-		 location = vl_colsubset(location, max_features);
-    end
+	%% in case not loading enough features
+	if dataPtr < max_features + 1,
+		data(:, dataPtr:end) = [];
+		location(:, dataPtr:end) = [];
+	end
 
 	output_dir = fileparts(output_file);
 	if ~exist(output_dir, 'file'),
 		mkdir(output_dir);
 	end
+	
+	fprintf('Totally [%d/%d] features loaded \n', dataPtr - 1, max_features);
 	
 	fprintf('Saving selected features to [%s]...\n', output_file);
 	hdf5write(output_file, '/data', data);
